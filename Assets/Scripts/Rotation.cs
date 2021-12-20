44881d3e12b596455dd1acb55f7c57f48eada220
Kosmik123 <malpa.w.klapkach@gmail.com>
2021-12-20 09:15:33 +0100

    Added rotation script

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public AnimationCurve moveCurve;
+    public float speed;
+
+    public float startAngle;
+    public float endAngle;
+
+    // states
+    private float progress;
+
+    private void Start()
+    {
+        progress = Random.Range(0f, 1f);
+    }
+
+    private void Update()
+    {
+        UpdateProgress();
+
+        float modifiedProgress = moveCurve.Evaluate(progress);
+
+        float currentAngle = Mathf.Lerp(startAngle, endAngle, modifiedProgress);
+        transform.localRotation = Quaternion.AngleAxis(currentAngle, Vector3.forward);
+    }
+
+    private void UpdateProgress()
+    {
+        progress += Time.deltaTime * speed;
+        if (progress >= 1)
+            progress = 0;
+    }
+}
