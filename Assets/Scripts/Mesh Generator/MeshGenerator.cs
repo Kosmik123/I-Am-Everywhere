3a795dadae09e980eddc75f049b9c7f1799eec39
Sollmyrr <karbos2004@gmail.com>
2022-01-05 20:09:31 +0100

    Added: test_scene

---
+++ Assets/Scripts/Mesh Generator/MeshGenerator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenerator : MonoBehaviour
+{
+    Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateMesh();
+        UpdateMesh();
+    }
+
+    private void CreateMesh()
+    {
+        vertices = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,1,0),
+            new Vector3(1,0,0)
+        };
+
+        triangles = new int[]
+        {
+            0,1,2
+            //1,3,2
+        };
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+}
